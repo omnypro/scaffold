baf37298f29fa0a164a061a0787cfaca45f3e7e7
Bryan Veloso <bryan@velo.so>
2025-05-30 21:41:06 -0700

    Initial commit.

---
+++ Scaffold/ScaffoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaffoldApp.swift
+//  Scaffold
+//
+//  Created by Bryan Veloso on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ScaffoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScaffoldTests/ScaffoldTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScaffoldTests.swift
+//  ScaffoldTests
+//
+//  Created by Bryan Veloso on 5/30/25.
+//
+
+import Testing
+
+struct ScaffoldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
