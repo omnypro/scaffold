23e395b4a6d15f4f7641ac44d6f8778a9ba4f1c3
Bryan Veloso <bryan@velo.so>
2025-05-31 17:01:05 -0700

    Add 1440p.

--- Scaffolde/Models/WindowSize.swift
+++ Scaffolde/Models/WindowSize.swift
@@ -9,6 +9,7 @@ struct WindowSize: Identifiable {
     static let presets: [WindowSize] = [
         WindowSize(name: "720p", width: 1280, height: 720),
         WindowSize(name: "1080p", width: 1920, height: 1080),
+        WindowSize(name: "1440p", width: 2560, height: 1440),
         WindowSize(name: "4K", width: 3840, height: 2160),
         WindowSize(name: "Square", width: 1080, height: 1080),
         WindowSize(name: "Vertical", width: 1080, height: 1920),
